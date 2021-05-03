b08c8ef1e97728087e764d7cbfdc56e032bd3589
jianghaoli201 <83488150+jianghaoli201@users.noreply.github.com>
2021-05-02 20:58:17 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+var arr = process.argv.slice(2);
+var dict = {
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Four',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine',
+    '0': 'Zero'
+}
+
+function convertWordToChar(word) {
+    var w;
+    if (word.length > 1) {
+        w = word.split("");
+    } else {
+        return Array(word);
+    }
+    return w;
+}
+
+
+function convertNum(arr) {
+    var res = '';
+    var splitArr = new Array(arr.length);
+
+    for (i = 0; i < arr.length; i++) {
+        splitArr[i] = convertWordToChar(arr[i]);
+    }
+
+    for (i = 0; i < splitArr.length; i++) {
+        for (j = 0; j < splitArr[i].length; j++) {
+            res += dict[splitArr[i][j]];
+        }
+        res += ',';
+    }
+    console.log(res)
+    return res
+}
+
+convertNum(arr);
